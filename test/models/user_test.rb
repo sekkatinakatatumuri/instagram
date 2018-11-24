088fc212095b6c18b5f48c6e4cc2a8dcf9f6a9b3
sekkatinakatatumuri <sekkatinakatatumuri@gmail.com>
2018-11-25 08:31:41 +0900

    add: Userモデルにパスワードに関するバリデーションを追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -36,6 +36,16 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+
   # フォーマットの検証
   test "email validation should accept valid addresses" do
     valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
