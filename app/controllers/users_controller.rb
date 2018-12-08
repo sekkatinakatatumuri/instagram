a10ffd588f8687b58a6b83b25602a1f36c187445
sekkatinakatatumuri <sekkatinakatatumuri@gmail.com>
2018-12-08 17:34:33 +0900

    add: Userコントローラーにeditアクションを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,8 +14,12 @@ class UsersController < ApplicationController
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
-      render 'new'
+      render :new
     end
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
   private
