cc0b2421b6820ee78dab2090a0fa8d3348253c6e
sekkatinakatatumuri <sekkatinakatatumuri@gmail.com>
2018-12-08 13:44:04 +0900

    add: ログイン画面の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   get 'sessions/new'
-  root             to: 'top#home'
-  get  '/signup',  to: 'users#new'
-  post '/signup',  to: 'users#create'
+  root              to: 'top#home'
+  get    '/signup', to: 'users#new'
+  post   '/signup', to: 'users#create'
+  get    '/login',  to: 'sessions#new'
+  post   '/login',  to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
 end
