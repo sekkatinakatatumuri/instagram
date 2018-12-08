95386704c6bafa33f11930b96b4105e273fb869f
sekkatinakatatumuri <sekkatinakatatumuri@gmail.com>
2018-12-08 14:55:55 +0900

    update: applicationコントローラーからprotected_from_forgeryの行をコメントアウト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
+  # https://stackoverflow.com/questions/50905654/rails-5-2-actioncontrollerinvalidauthenticitytoken
   include SessionsHelper
 end
